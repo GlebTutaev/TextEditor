76a018a3387877b61bc3d8391e703697aa5044a7
Tutaev Gleb <aka-laka-006@mail.ru>
2022-12-08 02:20:34 +0500

    Добавьте файлы проекта.

---
+++ PZ12/Create.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PZ12
+{
+    /// <summary>
+    /// Логика взаимодействия для Create.xaml
+    /// </summary>
+    public partial class Create : Window
+    {
+        public Create()
+        {
+            InitializeComponent();
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            
+
+        }
+
+        public string FileName
+        {
+            get { return fileNameBox.Text; }
+        }
+
+
+    }
+}
